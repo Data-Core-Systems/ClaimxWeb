a91f8260fba90554cc900e81123f6d637057f695
sourikdatta <sourik.datta@datacoresystems.com>
2014-12-04 18:34:16 -0500

    added

---
+++ BusinessObject/CL07.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimxWeb.BusinessObject
+{
+    public class CL07
+    {
+        public CL07()
+        { }
+
+
+        private string _cl07_user_id;
+        public string CL07_USER_ID { get { return _cl07_user_id; } set { _cl07_user_id = value; } }
+
+        private string _cl07_user_name;
+        public string CL07_USER_NAME { get { return _cl07_user_name; } set { _cl07_user_name = value; } }
+
+        private string _cl07_password;
+        public string CL07_PASSWORD { get { return _cl07_password; } set { _cl07_password = value; } }
+
+        private string _cl07_stat;
+        public string CL07_STAT { get { return _cl07_stat; } set { _cl07_stat = value; } }
+
+    }
+}
