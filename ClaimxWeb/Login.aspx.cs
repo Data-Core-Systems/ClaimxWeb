8da98a34c3d0517c7e215590994e49a520f7a6a0
sourikdatta <sourik.datta@datacoresystems.com>
2014-12-04 18:34:31 -0500

    added

---
+++ ClaimxWeb/Login.aspx.cs
@@ -0,0 +1,46 @@
+﻿using ClaimxWeb.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace ClaimxWeb
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static string doLogin(string username, string password)
+        {
+            
+            DataAccess.DataAccess da = new DataAccess.DataAccess();
+            ClaimxWeb.BusinessObject.CL07 UserData = da.DoLogin(username, password);
+           if(UserData!=null)
+           {
+               IList<CL01> JobDetails = getJobListbyUser(UserData.CL07_USER_ID);
+               return new JavaScriptSerializer().Serialize(JobDetails);
+               
+           }
+               
+            else
+                return "fail";
+        }
+
+        
+        public static IList<CL01> getJobListbyUser(string uid)
+        {
+            
+            DataAccess.DataAccess da = new DataAccess.DataAccess();
+            return (da.GetJobListByUser(uid));
+            
+        }
+    }
+}
