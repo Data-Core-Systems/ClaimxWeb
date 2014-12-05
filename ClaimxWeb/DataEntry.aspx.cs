5670b32eff6d17fee020f1ccfe2d5c5c2915236f
sourikdatta <sourik.datta@datacoresystems.com>
2014-12-05 16:36:07 -0500

    DataAccess removed

--- ClaimxWeb/DataEntry.aspx.cs
+++ ClaimxWeb/DataEntry.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using ClaimxWeb.DataAccess;
+
 using ClaimxWeb.BusinessObject;
 using System.IO;
 using System.Xml.Serialization;
